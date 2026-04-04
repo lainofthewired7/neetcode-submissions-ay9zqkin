5eee2ec3b46275e43ef21a14d4da57e3562f9ae1
lainofthewired7 <lamboedigin@gmail.com>
2026-04-03 23:21:09 -0400

    Add: decode-ways - submission-7

---
+++ Data Structures & Algorithms/decode-ways/submission-7.py
@@ -0,0 +1,42 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        if s[0] == "0":
+
+            return 0
+
+        n = len(s)
+
+        dp = [0] * n
+
+        dp[0] = 1
+
+        for i in range (1, n):
+
+            if s[i] != "0":
+
+                dp[i] += dp[i-1]
+
+            if int(s[i-1:i+1]) > 9 and int(s[i-1:i+1]) < 27:
+
+                if i == 1:
+
+                    dp[i] += 1
+
+                else:
+
+                    dp[i] += dp[i-2]
+
+        return dp[n-1]
+
+            
+
+
+        
+
+
+
+
+
+
+        
