9d802764b6bf504f5d453a1f0de41706d5c275ca
Lambert Edigin <lamboedigin@gmail.com>
2026-06-25 05:40:49 -0400

    Add: course-schedule - submission-12

---
+++ Data Structures & Algorithms/course-schedule/submission-12.py
@@ -0,0 +1,47 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        adj = {i:[] for i in range (numCourses)}
+
+        for prereq in prerequisites:
+
+            adj[prereq[0]].append(prereq[1])
+
+        seen = set()
+
+        path = set()
+
+        def dfs(course):
+
+            if course in seen:
+
+                return True
+
+            if course in path:
+
+                return False
+
+            path.add(course)
+
+            for neigh in adj[course]:
+
+                if not dfs(neigh):
+
+                    return False
+
+            path.remove(course)
+            
+            seen.add(course)
+
+            return True
+
+        for i in range (numCourses):
+
+            if not dfs(i):
+
+                return False
+
+        return True
+
+
+        
